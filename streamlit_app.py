ee4809133675be1788a6b6b75a761842c918eaad
Chris Baker <115461809+cpbakes@users.noreply.github.com>
2022-10-10 16:38:40 +0100

    Update streamlit_app.py
    Improve the visualisation of the Fruityvice data

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,9 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # Just writes data to the screen
+
+# Normalizes the response output
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display the table on the page
+streamlit.dataframe(fruityvice_normalized)
