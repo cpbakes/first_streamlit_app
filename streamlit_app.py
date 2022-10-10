dc061b5990114f22b786f39bef091d4e1a0cd943
Chris Baker <115461809+cpbakes@users.noreply.github.com>
2022-10-10 16:40:21 +0100

    Update streamlit_app.py
    Remove line and improve Fruityvice variable call

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # Just writes data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
-# Normalizes the response output
+# Take JSON version of response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Display the table on the page
+# Output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
